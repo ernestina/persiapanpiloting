a49d3c4698de1a0836a07098b6ca8040fd09c7ee
andi.saputra.jakarta@gmail.com <andi.saputra.jakarta@gmail.com@01e165f0-1a84-2ea0-30e5-300e9b2d8025>
2014-01-10 11:44:38 +0000

    mencoba memasukan data dalam view melalui kontroller

--- trunk/app/controllers/DataKppnController.php
+++ trunk/app/controllers/DataKppnController.php
@@ -26,14 +26,80 @@ class DataKppnController extends BaseController {
      * view Data KPPN PER KANWIL
      */
 
-    public function formIsian() {
+    public function formIsian($id = null) {
+        $d_kppn = new DataKppn($this->registry);
+        if (isset($_POST['add_d_kppn'])) {
+            $kd_d_user = $_POST['kd_d_user'];
+            $kd_d_tgl = $_POST['kd_d_tgl'];
+            $kd_d_pc = $_POST['kd_d_pc'];
+            $kd_d_pc_mas = $_POST['kd_d_pc_mas'];
+            $kd_d_laser = $_POST['kd_d_laser'];
+            $kd_d_laser_mas = $_POST['kd_d_laser_mas'];
+            $kd_d_dot = $_POST['kd_d_dot'];
+            $kd_d_dot_mas = $_POST['kd_d_dot_mas'];
+            $kd_d_supplier = $_POST['kd_d_supplier'];
+            $kd_d_supplier_mas = $_POST['kd_d_supplier_mas'];
+            $kd_d_kontrak = $_POST['kd_d_kontrak'];
+            $kd_d_kontrak_mas = $_POST['kd_d_kontrak_mas'];
+            $kd_d_saldo = $_POST['kd_d_saldo'];
+            $kd_d_saldo_mas = $_POST['kd_d_saldo_mas'];
+            $kd_d_retur = $_POST['kd_d_retur'];
+            $kd_d_retur_mas = $_POST['kd_d_retur_mas'];
+            $kd_d_konversi = $_POST['kd_d_konversi'];
+            $kd_d_konversi_mas = $_POST['kd_d_konversi_mas'];
+            $kd_d_supplier_tim = $_POST['kd_d_supplier_tim'];
+            $kd_d_supplier_tim_mas = $_POST['kd_d_supplier_tim_mas'];
+            $kd_d_kontrak_tim = $_POST['kd_d_kontrak_tim'];
+            $kd_d_kontrak_tim_mas = $_POST['kd_d_kontrak_tim_mas'];
+            $kd_d_user_id = $_POST['kd_d_user_id'];
+            $kd_d_user_id_mas = $_POST['kd_d_user_id_mas'];
+            $kd_d_input_by = $_POST['kd_d_input_by'];
+
+            $d_kppn->set_kd_d_user($kd_d_user);
+            $d_kppn->set_kd_d_tgl($kd_d_tgl);
+            $d_kppn->set_kd_d_pc($kd_d_pc);
+            $d_kppn->set_kd_d_pc_mas($kd_d_pc_mas);
+            $d_kppn->set_kd_d_laser($kd_d_laser);
+            $d_kppn->set_kd_d_laser_mas($kd_d_laser_mas);
+            $d_kppn->set_kd_d_dot($kd_d_dot);
+            $d_kppn->set_kd_d_dot_mas($kd_d_dot_mas);
+            $d_kppn->set_kd_d_supplier($kd_d_supplier);
+            $d_kppn->set_kd_d_supplier_mas($kd_d_supplier_mas);
+            $d_kppn->set_kd_d_kontrak($kd_d_kontrak);
+            $d_kppn->set_kd_d_kontrak_mas($kd_d_kontrak_mas);
+            $d_kppn->set_kd_d_saldo($kd_d_saldo);
+            $d_kppn->set_kd_d_saldo_mas($kd_d_saldo_mas);
+            $d_kppn->set_kd_d_retur($kd_d_retur);
+            $d_kppn->set_kd_d_retur_mas($kd_d_retur_mas);
+            $d_kppn->set_kd_d_konversi($kd_d_konversi);
+            $d_kppn->set_kd_d_konversi_mas($kd_d_konversi_mas);
+            $d_kppn->set_kd_d_supplier_tim($kd_d_supplier_tim);
+            $d_kppn->set_kd_d_supplier_tim_mas($kd_d_supplier_tim_mas);
+            $d_kppn->set_kd_d_kontrak_tim($kd_d_kontrak_tim);
+            $d_kppn->set_kd_d_kontrak_tim_mas($kd_d_kontrak_tim_mas);
+            $d_kppn->set_kd_d_user_id($kd_d_user_id);
+            $d_kppn->set_kd_d_user_id_mas($kd_d_user_id_mas);
+            $d_kppn->set_kd_d_input_by($kd_d_input_by);
+
+            if (!$d_kppn->add_d_kppn()) {
+                $this->view->d_rekam = $d_kppn;
+                $this->view->error = $d_kppn->get_error();
+            }
+        }
+        
+        if (!is_null($id)) {
+            $d_kppn->set_kd_d_kppn($id);
+            $this->view->d_ubah = $d_kppn->get_d_kppn($d_kppn);
+        }
+
+        $this->view->data = $d_kppn->get_d_kppn();
         $this->view->render('kppn/isianKppn');
     }
-    
+
     public function Level2() {
         $this->view->render('kppn/Lvl2');
     }
-    
+
     public function Level1() {
         $this->view->render('kppn/Lvl1');
     }
