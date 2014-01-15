c50b544e54b2d1f708a955d0296c5815a97c9b97
nez817@gmail.com <nez817@gmail.com@01e165f0-1a84-2ea0-30e5-300e9b2d8025>
2014-01-15 09:17:06 +0000

    look & feel rekap view+css

--- trunk/app/view/kppn/hasilKppn.php
+++ trunk/app/view/kppn/hasilKppn.php
@@ -8,106 +8,106 @@
         <table width="100%" class=" zebra">
             <thead>
                 <tr>
-                    <th rowspan="2">No</th>
-                    <th rowspan="2">Kantor</th>
-                    <th colspan="3">Jaringan</th>
-                    <th colspan="5">Data Awal (diisi KPPN)</th>
-                    <th colspan="3">Data Awal (Diisi Tim SPAN)</th>
-                    <th rowspan="2">TOTAL</th>
+                    <th rowspan="2" width="5%">No</th>
+                    <th rowspan="2" width="21%">Kantor</th>
+                    <th colspan="3" width="18%">Jaringan</th>
+                    <th colspan="5" width="30%">Data Awal (diisi KPPN)</th>
+                    <th colspan="3" width="18%">Data Awal (Diisi Tim SPAN)</th>
+                    <th rowspan="2" width="8%">TOTAL</th>
                 </tr>
                 <tr>
-                    <th>11</th>
-                    <th>12</th>
-                    <th>13</th>
-                    <th>21</th>
-                    <th>22</th>
-                    <th>23</th>
-                    <th>24</th>
-                    <th>25</th>
-                    <th>31</th>
-                    <th>32</th>
-                    <th>33</th>
+                    <th width="6%">11</th>
+                    <th width="6%">12</th>
+                    <th width="6%">13</th>
+                    <th width="6%">21</th>
+                    <th width="6%">22</th>
+                    <th width="6%">23</th>
+                    <th width="6%">24</th>
+                    <th width="6%">25</th>
+                    <th width="6%">31</th>
+                    <th width="6%">32</th>
+                    <th width="6%">33</th>
                 </tr>
             </thead>
             <?php
             foreach ($this->data as $val) {
                 if ($val->get_kd_d_user() >= 1000) {
-                    ?><tr>
+                    ?><tr style="text-align: center">
                         <td><?php echo $no++; ?></td>
-                        <td><?php echo $val->get_kd_d_kppn(); ?></td>
+                        <td style="text-align: left"><a href="#"><?php echo $val->get_kd_d_kppn(); ?></a></td>
                         <td><?php
                             if ($val->get_kd_d_pc() == 1) {
-                                echo "<icon class='icon-check'></icon>";
+                                echo "<icon class='icon-ok'></icon>";
                             } else {
                                 echo "<icon class='icon-remove'></icon>";
                             }
                             ?></td>
                         <td><?php
                             if ($val->get_kd_d_laser() == 1) {
-                                echo "<icon class='icon-check'></icon>";
+                                echo "<icon class='icon-ok'></icon>";
                             } else {
                                 echo "<icon class='icon-remove'></icon>";
                             }
                             ?></td>
                         <td><?php
                             if ($val->get_kd_d_dot() == 1) {
-                                echo "<icon class='icon-check'></icon>";
+                                echo "<icon class='icon-ok'></icon>";
                             } else {
                                 echo "<icon class='icon-remove'></icon>";
                             }
                             ?></td>
                         <td><?php
                             if ($val->get_kd_d_supplier() == 1) {
-                                echo "<icon class='icon-check'></icon>";
+                                echo "<icon class='icon-ok'></icon>";
                             } else {
                                 echo "<icon class='icon-remove'></icon>";
                             }
                             ?></td>
                         <td><?php
                             if ($val->get_kd_d_kontrak() == 1) {
-                                echo "<icon class='icon-check'></icon>";
+                                echo "<icon class='icon-ok'></icon>";
                             } else {
                                 echo "<icon class='icon-remove'></icon>";
                             }
                             ?></td>
                         <td><?php
                             if ($val->get_kd_d_saldo() == 1) {
-                                echo "<icon class='icon-check'></icon>";
+                                echo "<icon class='icon-ok'></icon>";
                             } else {
                                 echo "<icon class='icon-remove'></icon>";
                             }
                             ?></td>
                         <td><?php
                             if ($val->get_kd_d_retur() == 1) {
-                                echo "<icon class='icon-check'></icon>";
+                                echo "<icon class='icon-ok'></icon>";
                             } else {
                                 echo "<icon class='icon-remove'></icon>";
                             }
                             ?></td>
                         <td><?php
                             if ($val->get_kd_d_konversi() == 1) {
-                                echo "<icon class='icon-check'></icon>";
+                                echo "<icon class='icon-ok'></icon>";
                             } else {
                                 echo "<icon class='icon-remove'></icon>";
                             }
                             ?></td>
                         <td><?php
                             if ($val->get_kd_d_supplier_tim() == 1) {
-                                echo "<icon class='icon-check'></icon>";
+                                echo "<icon class='icon-ok'></icon>";
                             } else {
                                 echo "<icon class='icon-remove'></icon>";
                             }
                             ?></td>
                         <td><?php
                             if ($val->get_kd_d_kontrak_tim() == 1) {
-                                echo "<icon class='icon-check'></icon>";
+                                echo "<icon class='icon-ok'></icon>";
                             } else {
                                 echo "<icon class='icon-remove'></icon>";
                             }
                             ?></td>
                         <td><?php
                             if ($val->get_kd_d_user_id() == 1) {
-                                echo "<icon class='icon-check'></icon>";
+                                echo "<icon class='icon-ok'></icon>";
                             } else {
                                 echo "<icon class='icon-remove'></icon>";
                             }
