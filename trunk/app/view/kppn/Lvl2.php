1d90d5cf881eeb0276621bf846796cdea01cb34c
nez817@gmail.com <nez817@gmail.com@01e165f0-1a84-2ea0-30e5-300e9b2d8025>
2014-01-15 03:54:54 +0000

    look & feel kanwil v1 view

--- trunk/app/view/kppn/Lvl2.php
+++ trunk/app/view/kppn/Lvl2.php
@@ -1,18 +1,57 @@
-<h2>KONDISI KPPN DI <?php
+
+<div id="top"><br><br>
+<center><h2>KONDISI KPPN DI <?php
     foreach ($this->data_kanwil as $val_kanwil) {
         $nama_kanwil = $val_kanwil->get_kd_d_user();
         echo $nama_kanwil;
     }
     ?>
-</h2>
-<div id="top">
-
+</h2></center><br><br>
     <?php
     $jml_sukses = 0;
     $jml_kppn = 0;
     ?>
-                <?php foreach ($this->data as $val) { ?>
-        <div class = "kolomw">
+                
+    <!--tes coba-->
+	<div id="barat">
+		<table width="100%" class="zebra">
+			<?php foreach ($this->data as $val) { ?>
+			<tr>
+				<td width="70%" style="text-align: right; padding-top: 20px; "><h3><?php
+                echo $val->get_kd_d_kppn();
+                ?></h3></td>
+				<td width="30%"><?php
+                $all = $val->get_kd_d_pc() + $val->get_kd_d_laser() + $val->get_kd_d_dot() + $val->get_kd_d_supplier() + $val->get_kd_d_kontrak() + $val->get_kd_d_saldo() + $val->get_kd_d_retur() + $val->get_kd_d_konversi() + $val->get_kd_d_supplier_tim() + $val->get_kd_d_kontrak_tim() + $val->get_kd_d_user_id();
+                if ($all == 11) {
+                    echo "<h2 height = '200' width = '200'><div id = 'bundar' class = 'hijau' ></div></center></h2>";
+                    $jml_sukses++;
+                    $jml_kppn++;
+                } else {
+                    echo "<h2 height = '200' width = '200'><div id = 'bundar' class = 'merah' ></div></center></h2>";
+                    $jml_kppn++;
+                }
+                ?><!--h3 style="margin-top: -10px;"><?php echo $all ?>/11</h3></td-->
+			</tr>
+			<?php } ?>
+		</table>
+	</div>
+	<div id="timur">
+	  <div id="sidebar">
+		<h3>Persiapan Piloting SPAN di<br><?php echo $nama_kanwil; ?></h3>
+            <?php
+            if ($jml_sukses / $jml_kppn == 1) {
+                echo "<h2 height='200' width='200'><center><div id='bundar' class='hijau' ></div></center></h2>";
+            } else {
+                echo "<h2 height='200' width='200'><center><div id='bundar' class='merah' ></div></center></h2>";
+            }
+            ?>
+			<canvas id="canvas" height="200" width="200" style="padding-left: 10px"></canvas>
+            <center><h0><?php echo $jml_sukses; ?>/<?php echo $jml_kppn; ?></h0></center>
+        
+	  </div>
+	</div>
+	<!--tes coba-->
+		<!--div class = "kolomw">
             <fieldset><legend><?php
                 echo $val->get_kd_d_kppn();
                 ?></legend>
@@ -32,11 +71,11 @@
         </div>
 
         <?php
-    }
+    //}
     ?>
-    <div class="kolomv">
+    <!--div class="kolomv">
         <fieldset><legend>Resume KPPN di <?php echo $nama_kanwil; ?></legend>
-            <?php
+            <?php 
             if ($jml_sukses / $jml_kppn == 1) {
                 echo "<h2 height='200' width='200'><center>STATUS : <br> <div id='bundar' class='hijau' ></div></center></h2>";
             } else {
@@ -45,7 +84,7 @@
             ?>
             <center><h0><?php echo $jml_sukses; ?>/<?php echo $jml_kppn; ?></h0></center>
         </fieldset>
-    </div>
+    </div-->
 </div>
 
 <script>
@@ -56,6 +95,19 @@
                     //showOn: 'button'
         });
     });
+	var donat = [
+	{
+		value: 60,
+		color:"#E06666"
+	},
+	
+	{
+		value : 40,
+		color : "#70DB70"
+	}
+	]
+
+	var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Doughnut(donat);
 </script>
 
 
